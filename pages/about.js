9f3ee81323b9cee8ec1623e47e11515eb3f05038
Brian Mearns <bmearns@commercehub.com>
2020-01-23 21:33:45 -0500

    Next.js template with some basics

---
+++ pages/about.js
@@ -0,0 +1,34 @@
+import Header from "../components/Header";
+import React from "react";
+import axios from "axios";
+
+export default function About() {
+    const [appInfo, setAppInfoResp] = React.useState(null);
+    React.useEffect(() => {
+        (async () => {
+            try {
+                const resp = await axios.get("/api/app-info");
+                setAppInfoResp({ data: resp.data });
+            } catch (error) {
+                console.error("Error fetching app-info:", error);
+                setAppInfoResp({ error });
+            }
+        })();
+    });
+    return (
+        <div>
+            <Header />
+            <p>
+                {appInfo ? (
+                    appInfo.error ? (
+                        <p>An Error occurred fetching app-info</p>
+                    ) : (
+                        <pre>{JSON.stringify(appInfo.data, null, 4)}</pre>
+                    )
+                ) : (
+                    "Loading..."
+                )}
+            </p>
+        </div>
+    );
+}
